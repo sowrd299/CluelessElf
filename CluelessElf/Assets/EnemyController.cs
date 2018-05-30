ec79eb2fc2af7979ec91c9469d5bf9f0bf0f2edc
Unknown <molinakmey@gmail.com>
2018-05-30 16:56:01 -0700

    Programming: Enemy
    Adds enemy (which pushes player).
    Right wall bumps player to the left.

---
+++ CluelessElf/Assets/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public Collider2D collider;
+    public Vector2 force;
+    public int multiplier;
+    public int health;
+    public int deltaHealth;
+    //transform.position
+    // Use this for initialization
+    void Start()
+    {
+        force = transform.position;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionEnter2D used");
+
+        //test if player youches me
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Enemy bounces player back");
+            other.rigidbody.AddForce(multiplier * (other.rigidbody.position - force), ForceMode2D.Impulse);
+            health -= deltaHealth;
+            if(health <= 0)
+            {
+                Destroy(GetComponent<SpriteRenderer>());
+                Destroy(GetComponent<CircleCollider2D>());
+            }
+        }
+
+
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D used");
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+}
---
+++ CluelessElf/Assets/Scripts/BouncyWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyWall : MonoBehaviour {
+
+    public Collider2D collider;
+    public Vector2 force;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("OnCollisionEnter2D used");
+        other.rigidbody.AddForce(force, ForceMode2D.Impulse);
+
+    }
+
+    /*void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D used");
+    }*/
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
