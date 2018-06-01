40a7ee024c8c7d2b4282aa60cdd7dfd915b3b09d
Unknown <306489696@qq.com>
2018-06-01 04:37:48 -0700

    modified CameraFollow
    ...

--- CluelessElf/Assets/CameraFollow.cs
+++ CluelessElf/Assets/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour {
 
     public float smoothspeed = 0.125f;
     public Vector3 offset;
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 deiredPosition = target.position + offset;
         Vector3 smoothPosition = Vector3.Lerp(transform.position, deiredPosition, smoothspeed);
