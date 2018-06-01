6124c11b83054adb1135e6ace6c1b4dde82f9030
Unknown <306489696@qq.com>
2018-06-01 04:32:09 -0700

    CameraFollow works
    ...

---
+++ CluelessElf/Assets/Scripts/Movement_changed.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement_changed : MonoBehaviour
+{
+    public float _speed;
+
+    bool _defendstate;
+    private Rigidbody2D rb2d;
+
+    public float _original_speed = 20;
+    public float _slow = 10;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        _speed = 20;
+        _defendstate = false;
+    }
+
+    void Update()
+    {
+
+        
+
+        if (_defendstate != true){
+            if (Input.GetKeyDown(KeyCode.LeftControl)){
+                _speed = _slow;
+                _defendstate = true;
+            }
+        }
+        else{
+            if (Input.GetKeyUp(KeyCode.LeftControl)){
+                _speed = _original_speed;
+                _defendstate = false;
+            }
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+        Vector2 realmovement = new Vector2(moveX, moveY);
+        rb2d.AddForce(realmovement * _speed);
+        
+    }
+}
