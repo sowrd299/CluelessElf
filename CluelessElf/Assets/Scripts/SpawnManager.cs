888db95d8be40e1a714b56b7627411ce022772f0
Unknown <molinakmey@gmail.com>
2018-06-06 17:41:38 -0700

    Makes some changes.
    Creates a spawn manager than spawns moving enemies.
    Changed particles to black.

---
+++ CluelessElf/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+    public float spawnTime = 3f;
+    public GameObject mEnemy;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+    void Spawn()
+    {
+        GameObject.Instantiate(mEnemy, new Vector2(Random.Range(-45.0f, 45.0f), Random.Range(-45.0f, 45.0f)), Quaternion.identity);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
